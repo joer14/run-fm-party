4367322d3228636d20f9a45e37c5a2fac1dd3c71
Joe Rowley <joer14@gmail.com>
2017-12-10 18:27:40 -0800

    Cleans up footer, adds powered by strava logo

--- frontend/app/components/Footer/index.js
+++ frontend/app/components/Footer/index.js
@@ -5,38 +5,33 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components'
 import {
   Flex,
   Box,
-  Button,
+  Text,
 } from 'rebass'
 import messages from './messages';
 
-function Footer() {
+import poweredByStrava from './powered_by_strava.png';
 
-  const CustomButton = styled(Button)`
-    background: transparent;
-    color: black;
-  `
+function Footer() {
   return (
-    <Flex align='center' width={1}>
-      <CustomButton href='http://iha.org'>
-        <FormattedMessage {...messages.madeby} />
-      </CustomButton>
-      <Box mx='auto' />
-      <CustomButton href='https://travis-ci.org/jxnblk/rebass'>
-        <img
-          src='https://img.shields.io/travis/jxnblk/rebass/master.svg'
-          style={{
-            display: 'block',
-            margin: 0
-          }}
-        />
-      </CustomButton>
+    <Flex align='center' width={1} justify='space-between' style={{'cursor':'crosshair'}}>
+      <Box w={1/3} px={2}>
+    		<Text p={1} onClick={()=>{window.open('http://joerowley.net','_blank')}}>
+          <FormattedMessage {...messages.madeby} />
+    		</Text>
+    	</Box>
+    	<Box w={1/3} px={2}>
+        <a href='https://strava.com' target='_blank'>
+          <img height='34' href='https://strava.com' src={poweredByStrava}/>
+        </a>
+    	</Box>
+      <Box w={1/3} px={2}>
+      </Box>
     </Flex>
   );
 }
